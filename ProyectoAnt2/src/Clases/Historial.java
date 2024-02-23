49530d7122f9b312aea2fee9e8b51702b665edc3
bnitezzz <benitezb@correo.unimet.edu.ve>
2024-02-22 22:21:45 -0400

    Actualizando Clases Historial

--- ProyectoAnt2/src/Clases/Historial.java
+++ ProyectoAnt2/src/Clases/Historial.java
@@ -4,10 +4,51 @@
  */
 package Clases;
 
-/**
- *
- * @author maribelnunez
- */
+
 public class Historial {
+    private String ciclo;
+    private String recorridoHormigas;
+    private String caminoOptimo;
+    private double distanciaOptima;
+
+    public Historial(String ciclo, String recorridoHormigas, String caminoOptimo, double distanciaOptima) {
+        this.ciclo = ciclo;
+        this.recorridoHormigas = recorridoHormigas;
+        this.caminoOptimo = caminoOptimo;
+        this.distanciaOptima = distanciaOptima;
+    }
+
+    public String getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(String ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public String getRecorridoHormigas() {
+        return recorridoHormigas;
+    }
+
+    public void setRecorridoHormigas(String recorridoHormigas) {
+        this.recorridoHormigas = recorridoHormigas;
+    }
+
+    public String getCaminoOptimo() {
+        return caminoOptimo;
+    }
+
+    public void setCaminoOptimo(String caminoOptimo) {
+        this.caminoOptimo = caminoOptimo;
+    }
+
+    public double getDistanciaOptima() {
+        return distanciaOptima;
+    }
+
+    public void setDistanciaOptima(double distanciaOptima) {
+        this.distanciaOptima = distanciaOptima;
+    }
+    
     
 }
