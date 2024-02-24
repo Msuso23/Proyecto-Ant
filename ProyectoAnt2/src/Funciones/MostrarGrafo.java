719556f7a29cb1def5e8aff717e0ba83005030a9
Cristiandresgp <cristian.gouveia@correo.unimet.edu.ve>
2024-02-24 02:36:21 -0400

    Implementación de mostrar grafo con sus librerias. Arreglo de la pantalla de cargar txt. Arreglos de pequeños errores en guardar txt. Mensajes en clase hormiga puestos.

--- ProyectoAnt2/src/Funciones/MostrarGrafo.java
+++ ProyectoAnt2/src/Funciones/MostrarGrafo.java
@@ -4,10 +4,91 @@
  */
 package Funciones;
 
+import EDD.Arista;
+import EDD.Grafo;
+import EDD.Lista;
+import EDD.Nodo;
+import EDD.Vertice;
+import org.graphstream.graph.EdgeRejectedException;
+import org.graphstream.graph.ElementNotFoundException;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.IdAlreadyInUseException;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.Viewer;
+
 /**
  *
  * @author nicolepinto
  */
 public class MostrarGrafo {
-    
+
+    // Controlador de GraphStream
+    private Viewer viewer;
+
+    public void initGraphStream(Grafo myGraph) {
+        
+        Graph gs = createGraphStream(myGraph);
+        String styleSheet
+                = "edge {"
+                + "	size: 1px;"
+                + "	fill-color: #CCCCCC;"
+                + "	fill-mode: dyn-plain;"
+                + "     text-color: #ffffff;"
+                + "}"
+                + "node {"
+                + "	size: 12px;"
+                + "	fill-color: #990000;"
+                + "     text-color: #FFD700;"
+                + "}"
+                + "graph {"
+                + "     fill-color: #333333;"
+                + "}";
+
+        // GraphStream config
+        System.setProperty("org.graphstream.ui", "swing");
+        gs.setAttribute("ui.stylesheet", styleSheet);
+        gs.setAttribute("ui.quality");
+        gs.setAttribute("ui.antialias");
+        gs.display().setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+    }
+
+    private Graph createGraphStream(Grafo myGraph) {
+        Graph gs = new SingleGraph("Interfaz del grafo");
+        // ------------------------------
+        // Agregamos nodos/vertices
+        {
+            Nodo aux = myGraph.getListaCiudades().getpFirst();
+            while (aux != null) {
+                Vertice v = (Vertice) aux.getDato();
+                gs.addNode(v.toString()).setAttribute("ui.label", v.toString());
+                aux = aux.getPnext();
+            }
+        }
+
+        // Agregamos aristas
+        {
+            Nodo aux = myGraph.getListaCiudades().getpFirst();
+            while (aux != null) {
+                Vertice v = (Vertice) aux.getDato();
+                Lista lista_ady = v.getList_ady();
+                Nodo aux2 = lista_ady.getpFirst();
+                while (aux2 != null) {
+                    Arista arista = (Arista) aux2.getDato();
+                    String id = arista.getCiudadInicio().toString() + "-" + arista.getCiudadDestino().toString();
+
+                    try {
+                        gs.addEdge(id, arista.getCiudadInicio().toString(), arista.getCiudadDestino().toString()).setAttribute("ui.label", arista.getDistancia());
+                    } catch (EdgeRejectedException | ElementNotFoundException | IdAlreadyInUseException ex) {
+                    }
+
+                    // ----------
+                    aux2 = aux2.getPnext();
+                }
+                aux = aux.getPnext();
+            }
+        }
+
+        // ------------------------------
+        return gs;
+    }
 }
